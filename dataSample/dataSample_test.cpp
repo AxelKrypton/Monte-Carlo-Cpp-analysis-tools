da50164ea10dd3e4a05c7afc74ef11b708901cf0
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-03-07 14:18:58 +0100

    Added non-trivial test for mean to dataSample
    class

--- dataSample/dataSample_test.cpp
+++ dataSample/dataSample_test.cpp
@@ -19,6 +19,7 @@ BOOST_AUTO_TEST_CASE(build)
 
 BOOST_AUTO_TEST_CASE(mean1)
 {
+	// valarray initialises to zero
 	std::valarray<double> testValues(1);
 	double referenceValue = 0.;
 
@@ -34,6 +35,23 @@ BOOST_AUTO_TEST_CASE(mean2)
 {
 	std::valarray<double> testValues(1, 23);
 	double referenceValue = 1.;
+
+	dataSample * dataSampleInstance;
+	dataSampleInstance = new dataSample(testValues);
+	BOOST_REQUIRE(dataSampleInstance);
+
+	double mean = dataSampleInstance->getMean();
+	BOOST_CHECK_CLOSE(mean, referenceValue, testPrecision);
+}
+
+BOOST_AUTO_TEST_CASE(mean3)
+{
+	std::valarray<double> testValues(24);
+	for (int iteration = 0; iteration < testValues.size(); iteration ++)
+	{
+		testValues[iteration] = iteration + 1;
+	}
+	double referenceValue = 12.5;
 
 	dataSample * dataSampleInstance;
 	dataSampleInstance = new dataSample(testValues);
