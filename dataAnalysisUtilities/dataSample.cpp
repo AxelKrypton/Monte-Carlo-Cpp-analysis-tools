3451c0feac6a5030512cb0d9131dbf689a096c3d
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-03-25 12:10:19 +0100

    Moved FileReader class to own file

--- dataAnalysisUtilities/dataSample.cpp
+++ dataAnalysisUtilities/dataSample.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include "dataSample.hpp"
+#include "FileReader.hpp"
 
 DataSample::DataSample(int length)
 {
@@ -78,110 +79,6 @@ DataSample DataSample::applyFunction(double (*function)(double))
 	DataSample dataSample(functionAppliedToArray);
 	return dataSample;
 }
-
-static void checkIfDatafileExists(std::string filename)
-{
-	std::ifstream file;
-	file.open(filename.c_str());
-	if ( !file.is_open() )
-		throw std::invalid_argument("Given file \"" + filename + "\" does not exist!");
-	file.close();
-}
-
-static void checkIfColumnIsValid(int column)
-{
-	if(column <= 0)
-		throw std::invalid_argument("Number of column must be bigger than zero!");
-}
-
-static void checkIfOffsetIsValid(int offset)
-{
-	if(offset < 0)
-		throw std::invalid_argument("Offset must be greater than or equal to zero!");
-}
-
-class FileReader
-{
-public:
-	FileReader()
-	{
-		throw std::invalid_argument("Need input file to create FileReader!");
-	}
-	FileReader(std::string filename, int column, int offset):
-		filename(filename), column(column), offset(offset), lineCounter(0)
-	{
-		checkIfDatafileExists(filename);
-		checkIfColumnIsValid(column);
-		checkIfOffsetIsValid(offset);
-
-		infile.open(filename.c_str());
-	}
-
-	~FileReader()
-	{
-		infile.close();
-	}
-
-	DataSample readDataFromFile()
-	{
-		while (std::getline(infile, line))
-		{
-			if(line[0] != '#' && !( line.empty() ) ) //ignore comment or empty lines
-			{
-				if (!(lineCounter < offset))
-					extractContentFromLine();
-				lineCounter++;
-			}
-		}
-
-		checkOnFileStreamError();
-		checkIfReadWasSuccessful();
-		return DataSample( std::valarray<double>(data.data(), data.size()) );
-	}
-
-private:
-	void checkIfReadWasSuccessful()
-	{
-		if(data.size() == 0)
-		{
-			std::ostringstream ss ;
-			ss << column;
-			throw std::logic_error("datafile \"" + filename + "\", does not contain valid data in column " + ss.str() + "!");
-		}
-	}
-
-	void checkOnFileStreamError()
-	{
-		//todo: this is not covered in a test yet...
-		if(!(infile.peek() == EOF && infile.eof()) || infile.bad())
-		{
-			throw std::runtime_error("Error reading datafile");
-		}
-	}
-
-	void extractContentFromLine()
-	{
-		double currentNumber = 0.;
-		std::stringstream ss (line);
-		for(int i=0; i<column; i++)
-		{
-			if(ss >> currentNumber)
-			{
-				if(i==column-1)
-					data.push_back(currentNumber);
-			}
-		}
-	}
-
-	std::ifstream infile;
-	std::string line;
-	std::string filename;
-	std::vector<double> data;
-	double currentNumber;
-	int column;
-	int offset;
-	int lineCounter;
-};
 
 DataSample DataSample::readDataFromFile(std::string filename, int column, int offset)
 {
