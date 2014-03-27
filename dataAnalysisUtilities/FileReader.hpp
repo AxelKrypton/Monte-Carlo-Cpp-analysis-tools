8fff9934e39eec41154f8e0af6f84d011e6f3eb0
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2014-03-27 19:30:43 +0100

    Fixed a subtle error in FileReader and added a
    functionality to check for bad data entries

--- dataAnalysisUtilities/FileReader.hpp
+++ dataAnalysisUtilities/FileReader.hpp
@@ -34,7 +34,7 @@ public:
 		throw std::invalid_argument("Need input file to create FileReader!");
 	}
 	FileReader(std::string filename, int column, int offset):
-		filename(filename), column(column), offset(offset), lineCounter(0)
+		filename(filename), column(column), offset(offset), lineCounter(0), anyBadEntry(false)
 	{
 		checkIfDatafileExists(filename);
 		checkIfColumnIsValid(column);
@@ -48,13 +48,15 @@ public:
 		infile.close();
 	}
 
-	DataSample readDataFromFile()
+	DataSample readDataFromFile(bool* isAnyEntryBad = NULL)
 	{
 		goTroughFile();
 
 		checkOnFileStreamError();
 		checkIfReadWasSuccessful();
 
+		if(isAnyEntryBad != NULL)
+			*isAnyEntryBad = anyBadEntry;
 		return DataSample( std::valarray<double>(data.data(), data.size()) );
 	}
 
@@ -63,8 +65,9 @@ private:
 	{
 		while (std::getline(infile, line))
 		{
-			if(line[0] != '#' && !( line.empty() ) ) //ignore comment or empty lines
-			{
+			//ignore lines beginning by # since they are comments for gnuplot and empty lines
+			if(line.find_first_not_of("#\t ") < line.find_first_of("#")){
+				line = line.substr(0, line.find("#", 0)); //remove everything after '#' in each line
 				if (!(lineCounter < offset))
 					extractContentFromLine();
 				lineCounter++;
@@ -93,14 +96,23 @@ private:
 
 	void extractContentFromLine()
 	{
+		std::string currentEntry;
 		double currentNumber = 0.;
 		std::stringstream ss (line);
 		for(int i=0; i<column; i++)
 		{
-			if(ss >> currentNumber)
+			if(ss >> currentEntry)//TODO: think if this if is really needed
 			{
 				if(i==column-1)
-					data.push_back(currentNumber);
+				{
+					currentNumber = std::atof(currentEntry.c_str());
+					if(currentNumber != 0.0)
+						data.push_back(currentNumber);
+					else if(currentEntry.find_first_not_of("0.") != std::string::npos)
+					    anyBadEntry = true;
+					else
+						data.push_back(0.0);
+				}
 			}
 		}
 	}
@@ -113,6 +125,7 @@ private:
 	int column;
 	int offset;
 	int lineCounter;
+	bool anyBadEntry;
 };
 
 #endif /* FILEREADER_HPP_ */
