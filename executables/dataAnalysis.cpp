a319f1c77a58a8d3029c826bfbe33e5baacf89b4
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-04-07 10:58:27 +0200

    Refactoring of dataAnalysis executable

--- executables/dataAnalysis.cpp
+++ executables/dataAnalysis.cpp
@@ -1,11 +1,6 @@
-#include <boost/program_options.hpp>
-#include <boost/algorithm/string.hpp>
-namespace po = boost::program_options;
 
+#include "parameters.hpp"
 #include "../dataAnalysisUtilities/jackknifeEstimators.hpp"
-
-//todo: remove this again
-using namespace std;
 
 void calcAutocorrelation(std::string file)
 {
@@ -34,25 +29,25 @@ std::pair<double,double> calcMeanAndErrorOfDataSampleWithBinning(DataSampleAnaly
 
 void calcMean(std::string & file, int & numberOfBins)
 {
-    cout << "Analyse mean..." << endl;
+    std::cout << "Analyse mean..." << std::endl;
     DataSample sample(file);
 
     std::pair<double,double> meanAndError = calcMeanAndErrorOfDataSampleWithBinning(sample, numberOfBins);
 
-    cout << "Mean\t\tError" << endl;
-    cout << scientific << meanAndError.first << "\t" << meanAndError.second << endl;
+    std::cout << "Mean\t\tError" << std::endl;
+    std::cout << scientific << meanAndError.first << "\t" << meanAndError.second << std::endl;
 }
 
 void calcVariance(std::string & file, int & numberOfBins)
 {
-    cout << "Analyse variance..." << endl;
+    std::cout << "Analyse variance..." << std::endl;
     DataSampleAnalyzer sample(file);
     DataSampleAnalyzer varianceSample = (sample - sample.getNthMoment(1)) ^ 2;
 
     std::pair<double,double> meanAndError = calcMeanAndErrorOfDataSampleWithBinning(varianceSample, numberOfBins);
 
-    cout << "Variance\t\tError" << endl;
-    cout << scientific << meanAndError.first << "\t" << meanAndError.second << endl;
+    std::cout << "Variance\t\tError" << std::endl;
+    std::cout << scientific << meanAndError.first << "\t" << meanAndError.second << std::endl;
 }
 
 void calcSkewness(std::string & file, int & numberOfBins)
@@ -72,8 +67,8 @@ void calcSkewness(std::string & file, int & numberOfBins)
     JackknifeEstimators skewnessSample(jackSample1 / (jackSample2 ^ (3. / 2)));
     double skewness = skewnessSample.getNthMoment(1);
     double error = skewnessSample.getJackknifeError();
-    cout << "\t\tSkewness\t\tError" << endl;
-    cout << "JackEstimate:\t" << scientific << skewness << "\t" << error << endl;
+    std::cout << "\t\tSkewness\t\tError" << std::endl;
+    std::cout << "JackEstimate:\t" << scientific << skewness << "\t" << error << std::endl;
 }
 
 void calcKurtosis(std::string file, int numberOfBins)
@@ -96,83 +91,11 @@ void calcKurtosis(std::string file, int numberOfBins)
     JackknifeEstimators kurtosisSample(jackSample1 / (jackSample2 ^ 2));
     double skewness = kurtosisSample.getNthMoment(1);
     double error = kurtosisSample.getJackknifeError();
-    cout << "\t\tBinder\t\tError" << endl;
-    cout << "JackEstimate:\t" << scientific << skewness << "\t" << error << endl;
+    std::cout << "\t\tBinder\t\tError" << std::endl;
+    std::cout << "JackEstimate:\t" << scientific << skewness << "\t" << error << std::endl;
 }
 
-//todo: move to own file
-class parameters
-{
-public:
-	struct parse_aborted {};
-	int binsize;
-	int numberOfBins;
-	bool useBinning;
-	bool calcAutocorrelation;
-	int offset;
-	std::string file;
-	bool analyseMean;
-	bool analyseVariance;
-	bool analyseSkewness;
-	bool analyseKurtosis;
-	std::string defaultFile;
 
-	parameters(int argc, char ** argv)
-	{
-		defaultFile = "";
-
-		po::options_description desc("Generic options");
-		po::variables_map vm;
-
-		//todo: find out why short names gives parsing error!
-		//todo: add positional operator for data file!
-		desc.add_options()
-			("help,h", "Produce this help message")
-			("datafile,f", po::value<std::string>(&file)->default_value(defaultFile), "File containing data")
-			("offset,o", po::value<int>(&offset)->default_value(0), "Discard first <offset> values of data")
-			("analyseMean", po::value<bool>(&analyseMean)->default_value(true), "Analyse data for mean")
-			("analyseVariance", po::value<bool>(&analyseVariance)->default_value(true), "Analyse data for variance")
-			("analyseSkewness", po::value<bool>(&analyseSkewness)->default_value(true), "Analyse data for skewness")
-			("analyseKurtosis", po::value<bool>(&analyseKurtosis)->default_value(true), "Analyse data for kurtosis/binder-cumulant")
-			("useBinning", po::value<bool>(&useBinning)->default_value(true), "Use binning on data")
-			("numberOfBins,nb", po::value<int>(&numberOfBins)->default_value(10), "Number of bins")
-			("binsize,bs", po::value<int>(&binsize)->default_value(100), "Size of bin")
-			("calcAutocorrelation,ac", po::value<bool>(&calcAutocorrelation)->default_value(false), "Estimate autocorrelation of data data")
-		;
-
-		po::store(po::parse_command_line(argc, argv, desc), vm);
-
-		if(vm.count("help")) { // see http://stackoverflow.com/questions/5395503/required-and-optional-arguments-using-boost-library-program-options as to why this is done before po::notifiy(vm)
-			std::cout << desc << '\n';
-			throw parameters::parse_aborted();
-		}
-
-		po::notify(vm);
-		printParameters();
-	}
-
-private:
-	void printParameters()
-	{
-		cout << "###############################" << endl;
-		cout << "Options:" << endl;
-		cout << "###############################" << endl;
-		cout << "Datafile:\t" << file << endl;
-		cout << "Offset:\t" << offset << endl;
-		//todo: add output of observables which are analyzed
-		cout << "###############################" << endl;
-		if (useBinning)
-		{
-			cout << "binsize:\t" << binsize << endl;
-			cout << "number of bins:\t" << numberOfBins << endl;
-		}
-		else
-			cout << "Do not use binning!" << endl;
-		if (calcAutocorrelation)
-			cout << "Calculate estimate on autocorrelation" << endl;
-		cout << "###############################" << endl;
-	}
-};
 
 void dataAnalysis(parameters params)
 {
@@ -213,7 +136,7 @@ int main(int argc, char ** argv)
 	//todo: move catch block into own function?
 	catch ( const std::exception &e)
 	{
-		cout << e.what() << endl;
+		std::cout << e.what() << std::endl;
 	}
 	catch(parameters::parse_aborted)
 	{
@@ -221,7 +144,7 @@ int main(int argc, char ** argv)
 	}
 	catch (...)
 	{
-		cout << "Caught non-standard exception!";
+		std::cout << "Caught non-standard exception!";
 	}
 
     return 0;
---
+++ executables/parameters.hpp
@@ -0,0 +1,84 @@
+#ifndef PARAMETERS_HPP_
+#define PARAMETERS_HPP_
+
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+namespace po = boost::program_options;
+
+//todo: remove this again
+using namespace std;
+
+class parameters
+{
+public:
+	struct parse_aborted {};
+	int binsize;
+	int numberOfBins;
+	bool useBinning;
+	bool calcAutocorrelation;
+	int offset;
+	std::string file;
+	bool analyseMean;
+	bool analyseVariance;
+	bool analyseSkewness;
+	bool analyseKurtosis;
+	std::string defaultFile;
+
+	parameters(int argc, char ** argv)
+	{
+		defaultFile = "";
+
+		po::options_description desc("Generic options");
+		po::variables_map vm;
+
+		//todo: find out why short names gives parsing error!
+		//todo: add positional operator for data file!
+		desc.add_options()
+			("help,h", "Produce this help message")
+			("datafile,f", po::value<std::string>(&file)->default_value(defaultFile), "File containing data")
+			("offset,o", po::value<int>(&offset)->default_value(0), "Discard first <offset> values of data")
+			("analyseMean", po::value<bool>(&analyseMean)->default_value(true), "Analyse data for mean")
+			("analyseVariance", po::value<bool>(&analyseVariance)->default_value(true), "Analyse data for variance")
+			("analyseSkewness", po::value<bool>(&analyseSkewness)->default_value(true), "Analyse data for skewness")
+			("analyseKurtosis", po::value<bool>(&analyseKurtosis)->default_value(true), "Analyse data for kurtosis/binder-cumulant")
+			("useBinning", po::value<bool>(&useBinning)->default_value(true), "Use binning on data")
+			("numberOfBins,nb", po::value<int>(&numberOfBins)->default_value(10), "Number of bins")
+			("binsize,bs", po::value<int>(&binsize)->default_value(100), "Size of bin")
+			("calcAutocorrelation,ac", po::value<bool>(&calcAutocorrelation)->default_value(false), "Estimate autocorrelation of data data")
+		;
+
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+
+		if(vm.count("help")) { // see http://stackoverflow.com/questions/5395503/required-and-optional-arguments-using-boost-library-program-options as to why this is done before po::notifiy(vm)
+			std::cout << desc << '\n';
+			throw parameters::parse_aborted();
+		}
+
+		po::notify(vm);
+		printParameters();
+	}
+
+private:
+	void printParameters()
+	{
+		cout << "###############################" << endl;
+		cout << "Options:" << endl;
+		cout << "###############################" << endl;
+		cout << "Datafile:\t" << file << endl;
+		cout << "Offset:\t" << offset << endl;
+		//todo: add output of observables which are analyzed
+		cout << "###############################" << endl;
+		if (useBinning)
+		{
+			cout << "binsize:\t" << binsize << endl;
+			cout << "number of bins:\t" << numberOfBins << endl;
+		}
+		else
+			cout << "Do not use binning!" << endl;
+		if (calcAutocorrelation)
+			cout << "Calculate estimate on autocorrelation" << endl;
+		cout << "###############################" << endl;
+	}
+};
+
+#endif /* PARAMETERS_HPP_ */
