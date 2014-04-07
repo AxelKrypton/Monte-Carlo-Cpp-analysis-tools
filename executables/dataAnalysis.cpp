24a2ec8c8a51c3c95df3e541426583ebba170066
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-04-07 10:02:23 +0200

    Refactoring of dataAnalysis executable

--- executables/dataAnalysis.cpp
+++ executables/dataAnalysis.cpp
@@ -95,94 +95,107 @@ void calcKurtosis(std::string file, int numberOfBins)
     cout << "JackEstimate:\t" << scientific << skewness << "\t" << error << endl;
 }
 
-int main(int argc, char ** argv)
+class parameters
 {
-	//todo: add try-catch block
-
+public:
 	int binsize;
 	int numberOfBins;
 	bool useBinning;
 	bool calcAutocorrelation;
 	int offset;
 	std::string file;
-	bool analyseMean = true;
-	bool analyseVariance = true;
-	bool analyseSkewness = true;
-	bool analyseKurtosis = true;
-	std::string defaultFile = "";
+	bool analyseMean;
+	bool analyseVariance;
+	bool analyseSkewness;
+	bool analyseKurtosis;
+	std::string defaultFile;
 
-	po::options_description desc("Generic options");
-	po::variables_map vm;
-
-	//todo: find out why short names gives parsing error!
-	desc.add_options()
-		("help,h", "Produce this help message")
-		("datafile,f", po::value<std::string>(&file)->default_value(defaultFile), "File containing data")
-		("offset,o", po::value<int>(&offset)->default_value(0), "Discard first <offset> values of data")
-		("analyseMean", po::value<bool>(&analyseMean)->default_value(true), "Analyse data for mean")
-		("analyseVariance", po::value<bool>(&analyseVariance)->default_value(true), "Analyse data for variance")
-		("analyseSkewness", po::value<bool>(&analyseSkewness)->default_value(true), "Analyse data for skewness")
-		("analyseKurtosis", po::value<bool>(&analyseKurtosis)->default_value(true), "Analyse data for kurtosis/binder-cumulant")
-		("useBinning", po::value<bool>(&useBinning)->default_value(true), "Use binning on data")
-		("numberOfBins,nb", po::value<int>(&numberOfBins)->default_value(10), "Number of bins")
-		("binsize,bs", po::value<int>(&binsize)->default_value(100), "Size of bin")
-		("calcAutocorrelation,ac", po::value<bool>(&calcAutocorrelation)->default_value(false), "Estimate autocorrelation of data data")
-	;
-
-	po::store(po::parse_command_line(argc, argv, desc), vm);
-
-	if(vm.count("help")) { // see http://stackoverflow.com/questions/5395503/required-and-optional-arguments-using-boost-library-program-options as to why this is done before po::notifiy(vm)
-		std::cout << desc << '\n';
-		return 0;
-	}
-
-	po::notify(vm);
-
-	cout << "###############################" << endl;
-	cout << "Options:" << endl;
-	cout << "###############################" << endl;
-	cout << "Datafile:\t" << file << endl;
-	cout << "Offset:\t" << offset << endl;
-	//todo: add output of observables which are analyzed
-	cout << "###############################" << endl;
-	if (useBinning)
+	parameters(int argc, char ** argv)
 	{
-		cout << "binsize:\t" << binsize << endl;
-		cout << "number of bins:\t" << numberOfBins << endl;
-	}
-	else
-		cout << "Do not use binning!" << endl;
-	if (calcAutocorrelation)
-		cout << "Calculate estimate on autocorrelation" << endl;
-	cout << "###############################" << endl;
+		defaultFile = "";
 
-	if (file == defaultFile)
+		po::options_description desc("Generic options");
+		po::variables_map vm;
+
+		//todo: find out why short names gives parsing error!
+		desc.add_options()
+			("help,h", "Produce this help message")
+			("datafile,f", po::value<std::string>(&file)->default_value(defaultFile), "File containing data")
+			("offset,o", po::value<int>(&offset)->default_value(0), "Discard first <offset> values of data")
+			("analyseMean", po::value<bool>(&analyseMean)->default_value(true), "Analyse data for mean")
+			("analyseVariance", po::value<bool>(&analyseVariance)->default_value(true), "Analyse data for variance")
+			("analyseSkewness", po::value<bool>(&analyseSkewness)->default_value(true), "Analyse data for skewness")
+			("analyseKurtosis", po::value<bool>(&analyseKurtosis)->default_value(true), "Analyse data for kurtosis/binder-cumulant")
+			("useBinning", po::value<bool>(&useBinning)->default_value(true), "Use binning on data")
+			("numberOfBins,nb", po::value<int>(&numberOfBins)->default_value(10), "Number of bins")
+			("binsize,bs", po::value<int>(&binsize)->default_value(100), "Size of bin")
+			("calcAutocorrelation,ac", po::value<bool>(&calcAutocorrelation)->default_value(false), "Estimate autocorrelation of data data")
+		;
+
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+
+		if(vm.count("help")) { // see http://stackoverflow.com/questions/5395503/required-and-optional-arguments-using-boost-library-program-options as to why this is done before po::notifiy(vm)
+			std::cout << desc << '\n';
+			//todo: throw more suited exception
+			throw std::exception();
+		}
+
+		po::notify(vm);
+
+		cout << "###############################" << endl;
+		cout << "Options:" << endl;
+		cout << "###############################" << endl;
+		cout << "Datafile:\t" << file << endl;
+		cout << "Offset:\t" << offset << endl;
+		//todo: add output of observables which are analyzed
+		cout << "###############################" << endl;
+		if (useBinning)
+		{
+			cout << "binsize:\t" << binsize << endl;
+			cout << "number of bins:\t" << numberOfBins << endl;
+		}
+		else
+			cout << "Do not use binning!" << endl;
+		if (calcAutocorrelation)
+			cout << "Calculate estimate on autocorrelation" << endl;
+		cout << "###############################" << endl;
+
+	}
+};
+
+int main(int argc, char ** argv)
+{
+	parameters params(argc, argv);
+
+	//todo: add try-catch block
+
+	if (params.file == params.defaultFile)
 		return 0;
 
-	if (calcAutocorrelation)
+	if (params.calcAutocorrelation)
 	{
 		cout << "Autocorrelation is not implemented yet. Aborting!" << endl;
 		return 0;
 	}
 
-	if(analyseMean)
+	if(params.analyseMean)
 	{
-	    calcMean(file, numberOfBins);
+	    calcMean(params.file, params.numberOfBins);
 	}
 
-	if(analyseVariance)
+	if(params.analyseVariance)
 	{
-		calcVariance(file, numberOfBins);
+		calcVariance(params.file, params.numberOfBins);
 	}
 
-	if(analyseSkewness)
+	if(params.analyseSkewness)
 	{
-	    calcSkewness(file, numberOfBins);
+	    calcSkewness(params.file, params.numberOfBins);
 	}
 
-	if(analyseKurtosis)
+	if(params.analyseKurtosis)
 	{
-	    calcKurtosis(file, numberOfBins);
+	    calcKurtosis(params.file, params.numberOfBins);
 	}
 
   return 0;
