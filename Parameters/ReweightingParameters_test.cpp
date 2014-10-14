c1a45b1f1e22438ac1590929b81b7050a62102f2
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-10-14 14:34:52 +0200

    Added checks for setting parameter values

--- Parameters/ReweightingParameters_test.cpp
+++ Parameters/ReweightingParameters_test.cpp
@@ -66,3 +66,58 @@ BOOST_AUTO_TEST_SUITE(defaults)
 	}
 
 BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(setArguments)
+
+	BOOST_AUTO_TEST_CASE(filename1)
+	{
+		int numberOfArguments = 2;
+		std::string filename = "someName";
+		std::string filenameArgument = "-f" + filename;
+		const char * arguments[] = {"foo", filenameArgument.c_str()};
+		ReweightingParameters parameters(numberOfArguments, arguments);
+
+		BOOST_CHECK(filename == parameters.getInputfile());
+	}
+
+	BOOST_AUTO_TEST_CASE(filename2)
+	{
+		int numberOfArguments = 2;
+		std::string filename = "someName";
+		std::string filenameArgument = "--file=" + filename;
+		const char * arguments[] = {"foo", filenameArgument.c_str()};
+		ReweightingParameters parameters(numberOfArguments, arguments);
+
+		BOOST_CHECK(filename == parameters.getInputfile());
+	}
+
+	static ReweightingParameters createParametersForArgumentSettingCheck_longOption(std::string argumentName, int newValue)
+	{
+		std::string argument = argumentName + "=" + boost::lexical_cast<std::string>(newValue);
+		int numberOfArguments = 3;
+		const char * arguments[] = {"foo", "-f dummyFile", argument.c_str()};
+		return ReweightingParameters(numberOfArguments, arguments);
+	}
+
+	BOOST_AUTO_TEST_CASE(numberOfNewPoints)
+	{
+		uint newValue = 2;
+		std::string argumentName = "--numberOfNewPoints";
+		BOOST_REQUIRE_EQUAL(newValue, createParametersForArgumentSettingCheck_longOption(argumentName, newValue).getNumberOfNewPoints() );
+	}
+	
+	BOOST_AUTO_TEST_CASE(newRange_high)
+	{
+		double newValue = 65.;
+		std::string argumentName = "--newRange_high";
+		BOOST_REQUIRE_EQUAL(newValue, createParametersForArgumentSettingCheck_longOption(argumentName, newValue).getNewRange_high() );
+	}
+	
+	BOOST_AUTO_TEST_CASE(newRange_low)
+	{
+		double newValue = 14.;
+		std::string argumentName = "--newRange_low";
+		BOOST_REQUIRE_EQUAL(newValue, createParametersForArgumentSettingCheck_longOption(argumentName, newValue).getNewRange_low() );
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
