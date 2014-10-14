cba69a494cda9e1565dca1f20938099c72d1e0b6
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-10-14 14:21:22 +0200

    Added new Reweighting parameters

--- Parameters/ReweightingParameters.hpp
+++ Parameters/ReweightingParameters.hpp
@@ -1,12 +1,32 @@
 #ifndef REWEIGHTINGPARAMETERS_HPP_
 #define REWEIGHTINGPARAMETERS_HPP_
 
-#include "Parameters.hpp"
+#include "iostream"
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+namespace po = boost::program_options;
 
-class ReweightingParameters : public Parameters
+
+//TODO: Merge with other Parameters class
+class ReweightingParameters
 {
 public:
 	ReweightingParameters(int argc, const char ** argv);
+	
+	std::string getInputfile();
+	uint getNumberOfNewPoints();
+	double getNewRange_low();
+	double getNewRange_high();
+	
+	struct parse_aborted {};
+	
+private:
+	void checkParsedArguments(po::variables_map & vm, po::options_description & desc);
+	
+	std::string inputfile;
+	uint numberOfNewPoints;
+	double newRange_low;
+	double newRange_high;
 };
 
 #endif
--- Parameters/ReweightingParameters_test.cpp
+++ Parameters/ReweightingParameters_test.cpp
@@ -10,9 +10,59 @@ BOOST_AUTO_TEST_SUITE(ReweightingParameters_build)
 
 	BOOST_AUTO_TEST_CASE(invalidArgument)
 	{
+		int numberOfArguments = 1;
+		const char * argumentWithoutFile[] = {"foo"};
+		BOOST_REQUIRE_THROW(ReweightingParameters parameters(numberOfArguments, argumentWithoutFile), std::invalid_argument );
+	}
+	
+	BOOST_AUTO_TEST_CASE(build1)
+	{
 		int numberOfArguments = 2;
 		const char * argumentWithFile[] = {"foo", "someFilename"};
 		BOOST_CHECK_NO_THROW(ReweightingParameters parameters(numberOfArguments, argumentWithFile));
 	}
 	
+	BOOST_AUTO_TEST_CASE(help1)
+	{
+		int numberOfArguments = 2;
+		const char * argumentsWithHelp[] = {"foo", "-h"};
+		BOOST_REQUIRE_THROW(ReweightingParameters parameters(numberOfArguments, argumentsWithHelp), ReweightingParameters::parse_aborted );
+	}
+
+	BOOST_AUTO_TEST_CASE(help2)
+	{
+		int numberOfArguments = 2;
+		const char * argumentsWithHelp[] = {"foo", "--help"};
+		BOOST_REQUIRE_THROW(ReweightingParameters parameters(numberOfArguments, argumentsWithHelp), ReweightingParameters::parse_aborted );
+	}
+	
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(defaults)
+
+	static ReweightingParameters createParametersForDefaultCheck()
+	{
+		int numberOfArguments = 2;
+		const char * arguments[] = {"foo", "-f dummyFile"};
+		return ReweightingParameters(numberOfArguments, arguments);
+	}
+
+	BOOST_AUTO_TEST_CASE(numberOfNewPoints)
+	{
+		int defaultValue = 1;
+		BOOST_REQUIRE_EQUAL(defaultValue, createParametersForDefaultCheck().getNumberOfNewPoints() );
+	}
+	
+	BOOST_AUTO_TEST_CASE(newRange_high)
+	{
+		double defaultValue = 2.;
+		BOOST_REQUIRE_EQUAL(defaultValue, createParametersForDefaultCheck().getNewRange_high() );
+	}
+	
+	BOOST_AUTO_TEST_CASE(newRange_low)
+	{
+		double defaultValue = 1.;
+		BOOST_REQUIRE_EQUAL(defaultValue, createParametersForDefaultCheck().getNewRange_low() );
+	}
+
 BOOST_AUTO_TEST_SUITE_END()
