99eb00ed2d69db6d2a64e6c21fef0ef32e027192
Christopher Pinke <pinke@th.physik.uni-frankfurt.de>
2014-10-14 11:18:49 +0200

    Added dummy reweighting executable

---
+++ executables/reweighting.cpp
@@ -0,0 +1,33 @@
+#include "../Parameters/Parameters.hpp"
+#include "exceptions.hpp"
+#include "exitCodes.hpp"
+
+int main(int argc, const char ** argv)
+{
+	try
+	{
+		Parameters parameters(argc, argv);
+	}
+	//todo: move catch block into own function?
+	catch ( wrongBinningParameter &e)
+	{
+		std::cout << e.what() << std::endl;
+		exit(EXIT_BINNING_ERROR);
+	}
+	catch ( const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+		exit(EXIT_ERROR);
+	}
+	catch(Parameters::parse_aborted)
+	{
+		//do nothing in this case...
+	}
+	catch (...)
+	{
+		std::cout << "Caught non-standard exception!";
+		exit(EXIT_ERROR);
+	}
+
+    return EXIT_NORMAL;
+}
