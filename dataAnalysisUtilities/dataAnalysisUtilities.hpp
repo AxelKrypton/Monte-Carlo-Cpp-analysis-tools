bd3c660a09550b196d9be4b752a47aaca9ba004b
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2015-03-25 10:44:35 +0100

    Added some comments in dataAnalysisUtilities.hpp
    file

--- dataAnalysisUtilities/dataAnalysisUtilities.hpp
+++ dataAnalysisUtilities/dataAnalysisUtilities.hpp
@@ -5,14 +5,16 @@
 #include "../Parameters/Parameters.hpp"
 #include "EstimateAndError.hpp"
 
-EstimateAndError calcMeanAndErrorOfUncorrelatedDataSample(DataSample & sampleIn);
+//The following 4 functions DO BINNING(!) on sampleIn and then calculate the observable
 EstimateAndError calcMeanAndErrorOfDataSample(DataSample & sampleIn, Parameters parameters);
-EstimateAndError calcVarianceAndErrorOfUncorrelatedDataSample(DataSample & sampleIn);
 EstimateAndError calcVarianceAndErrorOfDataSample(DataSample & sampleIn, Parameters parameters);
 EstimateAndError calcSkewnessAndErrorOfDataSample(DataSample & sampleIn, Parameters parameters);
 EstimateAndError calcBinderAndErrorOfDataSample(DataSample & sampleIn, Parameters parameters);
-void calcAutocorrelationAndErrorOfDataSample(DataSample & sample, Parameters parameters);
+//The following two functions are not used in the rest of the project. Remove them!?
+EstimateAndError calcVarianceAndErrorOfUncorrelatedDataSample(DataSample & sampleIn);
+EstimateAndError calcMeanAndErrorOfUncorrelatedDataSample(DataSample & sampleIn);
 
+void calcAutocorrelationAndErrorOfDataSample(DataSample & sample, Parameters parameters);
 std::vector<EstimateAndError> calcArrayOfAutocorrelationFunctionsAndErrorEstimatesOfDataSample(DataSample & sample, Parameters parameters);
 std::vector<EstimateAndError> calcArrayOfAutocorrelationTimesAndErrorEstimatesOfDataSample(DataSample & sample, Parameters parameters);
 
