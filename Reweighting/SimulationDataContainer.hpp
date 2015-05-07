55f9444b2dd272fed5b6bc2d65b9547b2a0a7c16
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2015-05-07 18:45:01 +0200

    Adapted documentation of SimulationDataContainer
    class refs 73

--- Reweighting/SimulationDataContainer.hpp
+++ Reweighting/SimulationDataContainer.hpp
@@ -23,32 +23,35 @@ private:
 };
 
 /*
- * In the above class the method insertMomentsPerData is thought to facilitate the reweighting
- * of more complicated observables, like combinations of higher moments. We will use the
+ *  ATTENTION: The number of columns ranges from ZERO on.
+ *
+ * In the above class the method buildAndGetMomentsPerData is thought to facilitate the reweighting
+ * of more complicated quantities, like combinations of higher moments. We will use the
  * name MomentsPerData meaning the power of such data. For example, including in the container
  * the 3rd moment per data of the second column, then, per each SimulationData object in
  * the SimulationDataContainer, one column (i.e. a DataSampleBasic object) containing
  * (O2)^3 will be appended (where O2 is each entry of the second column).
- * The argument useMultipleColumnsForMoments is to decide how to build the higher moments.
- * Its size must be equal to that of whichColumns and its entries are referred in the obvious way
- * to the columns. If any entry is false then the central moments are build in the standard way,
- * otherwise the columns starting from that specified are used to build the higher moments; as
- * many as the highest required moment says. Notice that in case of using several columns, these
- * columns will be at the end substituted by one containing the mean line by line.
  *
- * Let's make an example -->  insertMomentsPerData({2,6},{2,3,4},{true, false});
+ * The argument columnsForWhichMultipleColumnsForMomentsAreUsed is to decide how to build the
+ * higher moments.Its entries are referred to the number of the columns. For the entries of this
+ * vector the columns starting from that specified are used to build the higher moments; as
+ * many as the highest required moment says (see further below for an example).
+ *
+ * The method works as follows: per each column of each file (except the first ignoreFirstNColumns),
+ * the moments specified in  whichMoments are build and appended at the end. After all the original
+ * columns (again except the first ignoreFirstNColumns) are deleted.
+ *
+ * Let's make an example -->  buildAndGetMomentsPerData({1,2,3,4}, 1, {2});
  * this call means:
  *  - using the four columns (four because the higher required moment is 4) starting from the number
- *    2, build the moments 2,3,4 that means:
+ *    2, build the moments 1,2,3,4 that means append at the end the four columns (needed for first moment
+ *    in reweighting) and then:
  *
  *     SECOND MOMENT = (col2 * col3 + col2 * col4 + col2 * col5 +  col3 * col4 + col3 * col5 + col4 * col5) / 6.
  *      THIRD MOMENT = (col2 * col3 * col4 + col3 * col4 * col5 +  col4 * col5 * col2 + col5 * col2 * col3) / 4.
  *     FOURTH MOMENT =  col2 * col3 * col4 * col5
  *
- *  - using the column number 6 build moments in the standard way
- *  - replace columns 2,3,4,5 with one containing the mean of them
- *
- * REMARK: The number of columns ranges from ZERO on.
+ *  - using all the other columns except the first (ignoreFirstNColumns==1), build moments in the standard way
  */
 
 
