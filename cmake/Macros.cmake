0915facca3c784b57daca6b0ab69d34f332cfc0f
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2020-01-14 14:59:08 +0100

    Add license notice to all code files
     Still the copyright statements have to be completed.

--- cmake/Macros.cmake
+++ cmake/Macros.cmake
@@ -1,21 +1,19 @@
-# Utility functions for specifying the build of OpTiMaL
 #
-# Copyright (c) 2012 Matthias Bach <bach@compeng.uni-frankfurt.de>
+#  Copyright (c)
 #
-# This file is part of CL2QCD.
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
-# CL2QCD is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
 #
-# CL2QCD is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
-# You should have received a copy of the GNU General Public License
-# along with CL2QCD.  If not, see <http://www.gnu.org/licenses/>.
 
 # Add a unit test
 # NAME name for the test to use in ctest
