8e196afdf1faa39a012301fbd067befec1bc888d
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2020-04-09 16:40:59 +0200

    Remove not-anymore-used code about old

--- dataAnalysisUtilities/dataAnalysisUtilities.hpp
+++ dataAnalysisUtilities/dataAnalysisUtilities.hpp
@@ -32,26 +32,6 @@
 
 realFloat evaluateErrorBasedOnMethod(DataSample, ErrorCalculationMethod);
 
-/*
- * The following 4 functions DO BINNING(!) on sampleIn and then calculate the observable
- *
- * ATTENTION: The fact that the functions below do binning means that they will modify
- *            the sampleIn cutting at the end some data. Then it is CRUCIAL to pass the
- *            sampleIn by value and not by reference (otherwise, in case of successive
- *            analysis the sample for the second analysis would be the output of the first,
- *            namely not the full data-set in the user file).
- */
-
-EstimateAndError calcMeanAndErrorOfDataSample(DataSample sampleIn, Parameters parameters);
-EstimateAndError calcVarianceAndErrorOfDataSample(DataSample sampleIn, Parameters parameters);
-EstimateAndError calcSkewnessAndErrorOfDataSample(DataSample sampleIn, Parameters parameters);
-EstimateAndError calcKurtosisAndErrorOfDataSample(DataSample sampleIn, Parameters parameters);
-// The following two functions are not used in the rest of the project. They are implemented here
-// for testing reasons (in tests binning is often not needed and it could be annoying to use the
-// functions above). TODO: Improve using the functions above in tests.
-EstimateAndError calcVarianceAndErrorOfUncorrelatedDataSample(DataSample& sampleIn, bool isMeanKnownToBeZero = false);
-EstimateAndError calcMeanAndErrorOfUncorrelatedDataSample(DataSample& sampleIn, bool isMeanKnownToBeZero = false);
-
 void calcAutocorrelationAndErrorOfDataSample(DataSample& sample, Parameters parameters);
 std::vector<EstimateAndError> calcArrayOfAutocorrelationFunctionsAndErrorEstimatesOfDataSample(DataSample& sample, Parameters parameters);
 std::vector<EstimateAndError> calcArrayOfAutocorrelationTimesAndErrorEstimatesOfDataSample(DataSample& sample, Parameters parameters);
